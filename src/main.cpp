1e53ab27efdef584dc6eb3d49c443c583616961c
Fyrmon Warslay <fyrmonwarslay@gmail.com>
2025-10-24 00:12:32 +0200

    Clean up main() a bit

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,5 @@
 int main()
 {
     FixedArray<int,3> a;
-    a.fill(15);
-    FixedArray<int,3> b;
-    b.fill(16);
-    std::cout<<(a == b) <<'\n';
-    std::swap(a,b);
-
-    std::cout<<"A: "<<a<<"\tB: "<<b<<'\n';
     return 0;
 }
