f43972e445e3116b37c622925413057b9dc492ec
Fyrmon Warslay <fyrmonwarslay@gmail.com>
2025-10-25 00:21:40 +0200

    Overload operator != and test it

--- lib/tests/arr_test.cpp
+++ lib/tests/arr_test.cpp
@@ -17,3 +17,36 @@ TEST_F(FixedArrayTest, CorrectSize)
   EXPECT_EQ(a.size(), 5);
   EXPECT_EQ(b.size(), 4);
 }
+
+TEST_F(FixedArrayTest, AccessingElements)
+{
+  EXPECT_EQ(a[0], 1);
+  EXPECT_EQ(b[1], 7);
+}
+
+TEST_F(FixedArrayTest, ChangingElements)
+{
+  a[0] = 99;
+  EXPECT_EQ(a[0], 99);
+
+  b[1] = 33;
+  EXPECT_EQ(b[1], 33);
+}
+
+TEST_F(FixedArrayTest, EqualityCheck)
+{
+  TestArrA arr{1,2,3,4,5};
+  EXPECT_EQ( a, arr);
+
+  arr[0] = 12;
+  EXPECT_NE(a, arr);
+}
+
+TEST_F(FixedArrayTest, InequalityCheck)
+{
+  TestArrA arr{6,7,8,9,10};
+  EXPECT_NE( a, arr);
+
+  TestArrA b{1,2,3,4,5};
+  EXPECT_EQ( a != b, 0);
+}
